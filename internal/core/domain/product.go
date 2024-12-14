064e310e96bcca56fff3cf2d677ce4203901b29a
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-12-14 15:03:37 -0500

    feat: add JSON annotations to domain structs
    Added JSON annotations to the Category and Product structs for better JSON serialization. This ensures compatibility with external systems and improves API usability.

--- internal/core/domain/product.go
+++ internal/core/domain/product.go
@@ -3,14 +3,14 @@ package domain
 import "time"
 
 type Product struct {
-	ID          uint64
-	Name        string
-	Description string
-	Price       float64
-	Active      bool
-	CategoryID  uint64
-	Category    Category
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uint64    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	Active      bool      `json:"active"`
+	CategoryID  uint64    `json:"categoryID"`
+	Category    Category  `json:"category"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 	// TODO: add staff id
 }
