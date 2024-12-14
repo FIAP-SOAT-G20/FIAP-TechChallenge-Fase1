8ae2182cc8e7ed70b8a5150e2e875eb46da859dc
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-12-14 13:49:12 -0500

    feat(database): add initial PostgreSQL schema migrations
    Introduce migration scripts to create and drop initial database tables for customers, orders, products, categories, payments, and related entities. The `.up.sql` file builds the schema, while the `.down.sql` file handles its rollback.

---
+++ internal/adapter/storage/postgres/migrations/000001_create_initials_tables.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE order_product;
+DROP TABLE order_history;
+DROP TABLE orders;
+DROP TABLE payments;
+DROP TABLE products;
+DROP TABLE staffs;
+DROP TABLE categories;
+DROP TABLE customers;
---
+++ internal/adapter/storage/postgres/migrations/000001_create_initials_tables.up.sql
@@ -0,0 +1,74 @@
+CREATE TABLE customers
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR   NOT NULL,
+    email      VARCHAR   NOT NULL UNIQUE,
+    cpf        VARCHAR   NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE categories
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR   NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE staffs
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    role VARCHAR CHECK (role IN ('COOK', 'ATTENDANT', 'MANAGER'))
+);
+
+CREATE TABLE products
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR        NOT NULL,
+    description VARCHAR,
+    price       DECIMAL(19, 2) NOT NULL,
+    category_id INT            NOT NULL REFERENCES categories (id),
+    image_url   VARCHAR,
+    staff_id    INT REFERENCES staffs (id),
+    active      BOOLEAN                 DEFAULT true,
+    created_at  TIMESTAMP      NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMP      NOT NULL DEFAULT now()
+);
+
+CREATE TABLE payments
+(
+    id         SERIAL PRIMARY KEY,
+    status     VARCHAR CHECK (status IN ('PENDING', 'CONFIRMED', 'CANCELED')) DEFAULT 'PENDING',
+    created_at TIMESTAMP NOT NULL                                             DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL                                             DEFAULT now()
+);
+
+CREATE TABLE orders
+(
+    id          SERIAL PRIMARY KEY,
+    payment_id  INT REFERENCES payments (id),
+    customer_id INT REFERENCES customers (id),
+    total_bill  DECIMAL(19, 2),
+    created_at  TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE order_history
+(
+    order_id   INT REFERENCES orders (id),
+    staff_id   INT REFERENCES staffs (id),
+    status     VARCHAR CHECK (status IN ('RECEIVED', 'PREPARING', 'READY', 'COMPLETED')) DEFAULT 'RECEIVED',
+    created_at TIMESTAMP NOT NULL                                                        DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL                                                        DEFAULT now(),
+    PRIMARY KEY (order_id, staff_id)
+);
+
+CREATE TABLE order_product
+(
+    order_id   INT REFERENCES orders (id),
+    product_id INT REFERENCES products (id),
+    price      DECIMAL(19, 2),
+    quantity   INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+);
