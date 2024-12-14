ead6ca8225a2b4a7bfb82c223aed35a0cb1e93b1
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-12-14 14:46:00 -0500

    feat(category): add CategoryRepository with CRUD operations
    Introduced a new `CategoryRepository` for handling category-related database interactions in the PostgreSQL storage layer. This includes methods for inserting, fetching by ID, and paginated retrieval of categories. Updated the `GetAll` method signature in the CategoryRepository interface to align with the new implementation.

---
+++ internal/adapter/storage/postgres/repository/category_repository.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/core/domain"
+)
+
+type CategoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) *CategoryRepository {
+	return &CategoryRepository{db}
+}
+
+func (r *CategoryRepository) Insert(category *domain.Category) error {
+	return r.db.Create(category).Error
+}
+
+func (r *CategoryRepository) GetByID(id uint64) (*domain.Category, error) {
+	var category domain.Category
+	err := r.db.First(&category, id).Error
+	return &category, err
+}
+
+func (r *CategoryRepository) GetAll(name string, page, limit int) ([]domain.Category, int64, error) {
+	var categories []domain.Category
+	var count int64
+
+	query := r.db.Model(&domain.Category{})
+
+	if name != "" {
+		query = query.Where("name LIKE ?", "%"+name+"%")
+	}
+
+	query.Count(&count)
+
+	err := query.Offset((page - 1) * limit).Limit(limit).Find(&categories).Error
+
+	return categories, count, err
+}
