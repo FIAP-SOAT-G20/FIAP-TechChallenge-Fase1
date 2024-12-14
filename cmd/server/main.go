e9849cd01f683016b4c2e22baa440b482a516f6f
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-12-14 15:02:38 -0500

    feat(server): add main server entry point implementation
    Introduce the main entry point for the server, including environment configuration, database connection, and router setup. This implementation initializes repositories, services, handlers, and starts the HTTP server.

---
+++ cmd/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/config"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/http/handler"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/http/router"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/storage/postgres"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/storage/postgres/repository"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/core/service"
+)
+
+func main() {
+	environment, err := config.LoadEnvironment()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	database, err := postgres.Connect(environment.DatabaseURL)
+	if err != nil {
+		log.Fatalln("failed to connect database", err)
+	}
+
+	// repositories
+	categoryRepository := repository.NewCategoryRepository(database)
+	productRepository := repository.NewProductRepository(database)
+
+	// services
+	productServive := service.NewProductService(productRepository, categoryRepository)
+
+	// handlers
+	productHandler := handler.NewProductHandler(productServive)
+
+	// router
+	routes := router.NewRouter(productHandler)
+	if err := routes.Run(":" + environment.Port); err != nil {
+		log.Fatalln("failed to run server", err)
+	}
+}
