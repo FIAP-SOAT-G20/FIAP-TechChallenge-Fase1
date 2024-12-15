e7097bfcedc78788eebd3d0cd143d7736bbb837d
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-12-15 08:15:00 -0500

    fix(customer): update timestamp fields to time.Time

--- internal/core/domain/customer.go
+++ internal/core/domain/customer.go
@@ -1,10 +1,12 @@
 package domain
 
+import "time"
+
 type Customer struct {
-	ID        uint64 `json:"id"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	CPF       string `json:"cpf"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
+	ID        uint64    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CPF       string    `json:"cpf"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
