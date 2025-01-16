520aaa3ccb729449b0502d6e3bbf2adee9b57eb4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2025-01-16 13:44:48 -0300

    Test: Payment (#18)
    * feat: add mock implementations for external payment and order repositories
    
    * test: add unit tests for PaymentService methods
    
    * docs: update README with environment variable instructions and health check details
    
    * docs: update README with additional resources and clarify coverage report instructions
    
    * docs: enhance README and API documentation with additional endpoints and descriptions
    
    * docs: update API documentation with enhanced descriptions and external links

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"context"
+	"log"
+	"log/slog"
+
 	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal"
 	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/config"
 	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/adapter/logger"
-	"log"
-	"log/slog"
 )
 
-//	@title			FIAP Tech Challenge Fase 1 - G20 - 10 SOAT
+//	@title			FIAP Tech Challenge Fase 1 - 10SOAT - G20
 //	@version		1
-//	@description	API para o Tech Challenge da FIAP - Fase 1 - G20 - 10 SOAT
-
+//	@description	API de um Fast Food para o Tech Challenge da FIAP - Fase 1 - 10SOAT - G20
+//	@servers		[ { "url": "http://localhost:8080" } ]
+//	@host			localhost:8080
+//	@BasePath		/api/v1
+//	@tag.name sign-up
+//	@tag.description 2.b: i.Cadastro do Cliente
+//	@tag.name products
+//	@tag.description 2.b: iii. Criar, editar e remover produtos;
+//	@tag.name payments
+//	@tag.description 2.b: v. Fake checkout
+//	@tag.name sign-in
+//	@tag.description 2.b: ii. Identificação do Cliente via CPF
+// @externalDocs.description GitHub Repository
+// @externalDocs.url https://github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1
+//
 // @securityDefinitions.apikey	BearerAuth
 // @in							header
 // @name						Authorization
---
+++ internal/core/service/payment_service_test.go
@@ -0,0 +1,397 @@
+package service
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/core/domain"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/core/port"
+	"github.com/FIAP-SOAT-G20/FIAP-TechChallenge-Fase1/internal/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestNewPaymentService(t *testing.T) {
+	type args struct {
+		paymentRepository      port.IPaymentRepository
+		orderRepository        port.IOrderRepository
+		externalPaymentService port.IExternalPaymentService
+	}
+	tests := []struct {
+		name string
+		args args
+		want *PaymentService
+	}{
+		{
+			name: "TestNewPaymentService",
+			args: args{
+				paymentRepository:      nil,
+				orderRepository:        nil,
+				externalPaymentService: nil,
+			},
+			want: &PaymentService{
+				paymentRepository:      nil,
+				orderRepository:        nil,
+				externalPaymentService: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewPaymentService(tt.args.paymentRepository, tt.args.orderRepository, tt.args.externalPaymentService); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPaymentService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPaymentService_CreatePayment(t *testing.T) {
+	mockPaymentRepository := new(mocks.MockPaymentRepository)
+	mockOrderRepository := new(mocks.MockOrderRepository)
+	mockExternalPaymentService := new(mocks.MockExternalPaymentService)
+
+	type args struct {
+		orderID uint64
+	}
+	tests := []struct {
+		name       string
+		args       args
+		setupMocks func()
+		want       *domain.Payment
+		wantErr    bool
+	}{
+		{
+			name: "Success - Existing Pending Payment",
+			args: args{
+				orderID: 0,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(0)).
+					Return(&domain.Payment{
+						ID: 1,
+					}, nil)
+			},
+			want: &domain.Payment{
+				ID: 1,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Success - New Payment",
+			args: args{
+				orderID: 1,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(1)).
+					Return(&domain.Payment{
+						ID: 0,
+					}, nil)
+
+				mockOrderRepository.
+					On("GetByID", uint64(1)).
+					Return(&domain.Order{ID: 1}, nil)
+
+				mockExternalPaymentService.
+					On("CreatePaymentMock", &domain.CreatePaymentIN{
+						ExternalReference: "1",
+						TotalAmount:       0,
+						Items:             nil,
+						Title:             "FIAP Tech Challenge - Product Order",
+						Description:       "Purchases made at the FIAP Tech Challenge store",
+						NotificationUrl:   "",
+					}).
+					Return(&domain.CreatePaymentOUT{
+						InStoreOrderID: "123",
+						QrData:         "456",
+					}, nil)
+
+				mockPaymentRepository.
+					On("Insert", &domain.Payment{
+						Status:            domain.PROCESSING,
+						ExternalPaymentID: "123",
+						OrderID:           1,
+						QrData:            "456",
+					}).
+					Return(&domain.Payment{ID: 1}, nil)
+			},
+			want: &domain.Payment{
+				ID: 1,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Error - GetPaymentByOrderIDAndStatus",
+			args: args{
+				orderID: 0,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(0)).
+					Return(nil, assert.AnError)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Error - GetByID",
+			args: args{
+				orderID: 1,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(1)).
+					Return(&domain.Payment{
+						ID: 0,
+					}, nil)
+
+				mockOrderRepository.
+					On("GetByID", uint64(1)).
+					Return(nil, assert.AnError)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Error - CreatePaymentMock",
+			args: args{
+				orderID: 1,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(1)).
+					Return(&domain.Payment{
+						ID: 0,
+					}, nil)
+
+				mockOrderRepository.
+					On("GetByID", uint64(1)).
+					Return(&domain.Order{ID: 1}, nil)
+
+				mockExternalPaymentService.
+					On("CreatePaymentMock", &domain.CreatePaymentIN{
+						ExternalReference: "1",
+						TotalAmount:       0,
+						Items:             nil,
+						Title:             "FIAP Tech Challenge - Product Order",
+						Description:       "Purchases made at the FIAP Tech Challenge store",
+						NotificationUrl:   "",
+					}).
+					Return(nil, assert.AnError)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Error - Insert - Payment",
+			args: args{
+				orderID: 1,
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("GetPaymentByOrderIDAndStatus", domain.PROCESSING, uint64(1)).
+					Return(&domain.Payment{ID: 0}, nil)
+
+				mockPaymentRepository.
+					On("Insert", mock.Anything).
+					Return(nil, assert.AnError)
+
+				mockOrderRepository.
+					On("GetByID", mock.Anything).
+					Return(&domain.Order{ID: 1}, nil)
+
+				mockExternalPaymentService.
+					On("CreatePaymentMock", mock.Anything).
+					Return(&domain.CreatePaymentOUT{
+						InStoreOrderID: "123",
+						QrData:         "456",
+					}, nil)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			t.Cleanup(func() {
+				mockExternalPaymentService.ExpectedCalls = nil
+				mockOrderRepository.ExpectedCalls = nil
+				mockPaymentRepository.ExpectedCalls = nil
+			})
+			tt.setupMocks()
+			ps := NewPaymentService(
+				mockPaymentRepository,
+				mockOrderRepository,
+				mockExternalPaymentService,
+			)
+
+			// Act
+			got, err := ps.CreatePayment(tt.args.orderID)
+
+			// Assert
+			if (err != nil) != tt.wantErr {
+				assert.Error(t, err)
+				return
+				// t.Errorf("PaymentService.CreatePayment() error = %v, wantErr %v", err, tt.wantErr)
+				// return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				assert.Equal(t, tt.want, got)
+				// t.Errorf("PaymentService.CreatePayment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPaymentService_createPaymentPayload(t *testing.T) {
+	type args struct {
+		order *domain.Order
+	}
+	tests := []struct {
+		name string
+		args args
+		want *domain.CreatePaymentIN
+	}{
+		{
+			name: "Success",
+			args: args{
+				order: &domain.Order{
+					ID: 1,
+					OrderProducts: []domain.OrderProduct{
+						{
+							ProductID: 1,
+							Price:     10,
+							Quantity:  1,
+						},
+					},
+				},
+			},
+			want: &domain.CreatePaymentIN{
+				ExternalReference: "1",
+				TotalAmount:       0,
+				Items: []domain.ItemsIN{
+					{
+						Title:       "",
+						Description: "",
+						UnitPrice:   0,
+						Category:    "marketplace",
+						UnitMeasure: "unit",
+						Quantity:    1,
+						TotalAmount: 10,
+					},
+				},
+				Title:           "FIAP Tech Challenge - Product Order",
+				Description:     "Purchases made at the FIAP Tech Challenge store",
+				NotificationUrl: "",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			ps := NewPaymentService(nil, nil, nil)
+
+			// Act
+			if got := ps.createPaymentPayload(tt.args.order); !reflect.DeepEqual(got, tt.want) {
+
+				// Assert
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestPaymentService_UpdatePayment(t *testing.T) {
+	mockPaymentRepository := new(mocks.MockPaymentRepository)
+
+	type args struct {
+		payment *domain.UpdatePaymentIN
+	}
+	tests := []struct {
+		name       string
+		args       args
+		setupMocks func()
+		want       *domain.Payment
+		wantErr    bool
+	}{
+		{
+			name: "Success",
+			args: args{
+				payment: &domain.UpdatePaymentIN{
+					Resource: "123",
+					Topic:    "topic",
+				},
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("UpdateStatus", domain.CONFIRMED, "123").
+					Return(nil)
+
+				mockPaymentRepository.
+					On("GetByExternalPaymentID", "123").
+					Return(&domain.Payment{
+						ID: 1,
+					}, nil)
+			},
+			want:    &domain.Payment{ID: 1},
+			wantErr: false,
+		},
+		{
+			name: "Error - UpdateStatus",
+			args: args{
+				payment: &domain.UpdatePaymentIN{
+					Resource: "123",
+					Topic:    "topic",
+				},
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("UpdateStatus", domain.CONFIRMED, "123").
+					Return(assert.AnError)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "Error - GetByExternalPaymentID",
+			args: args{
+				payment: &domain.UpdatePaymentIN{
+					Resource: "123",
+					Topic:    "topic",
+				},
+			},
+			setupMocks: func() {
+				mockPaymentRepository.
+					On("UpdateStatus", domain.CONFIRMED, "123").
+					Return(nil)
+
+				mockPaymentRepository.
+					On("GetByExternalPaymentID", "123").
+					Return(nil, assert.AnError)
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			t.Cleanup(func() {
+				mockPaymentRepository.ExpectedCalls = nil
+			})
+			tt.setupMocks()
+			ps := NewPaymentService(mockPaymentRepository, nil, nil)
+
+			got, err := ps.UpdatePayment(tt.args.payment)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PaymentService.UpdatePayment() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PaymentService.UpdatePayment() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
